ed13f2947a9a32021a98c4c98c476d5cce5f1781
TRẦN HUY PHƯỢNG <1712685@student.hcmus.edu.vn>
2019-09-17 18:52:31 +0700

    newpro

---
+++ Project2/Source.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<string>
+#include<string.h>
+#include<algorithm>
+#include<cmath>
+#include<map>
+#include<cstdlib>
+#include<vector>
+#include<set>
+#include<deque>
+#include<queue>
+#include<math.h>
+#include<chrono>
+#include<list>
+#include<random>
+#include<bitset>
+#include<assert.h>
+#include<ctime>
+#include<cstdio>
+#include <unordered_map>
+using namespace std;
+#define fast_input() {ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); }
+#define ll long long
+#define For(i,n) for (int(i) = 0; (i)< (int)(n); (i)++)
+#define For1(i,n) for (int(i) = 1; (i) <= (int)(n); (i)++)
+#define Forlr(i,l,r) for (int(i) = (int)(l); (i) <= (int)(r); (i)++)
+#define Forrl(i,r,l) for (int(i) = (int)(r); (i) >= (int)(l); (i)--)
+#define all(a) a.begin(), a.end() 
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define PI 3.14159265
+#define putno return cout <<"NO", 0;
+#define putyes return cout <<"YES", 0;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vii;
+typedef vector<ll> vl;
+typedef map<int, int> mii;
+//const ll MOD = 1e9 + 7;
+const int N = 1e5 + 5;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+/*
+MIKUNO - 15/02/1999
+*/
+int n, m, k, cnt, rs;
+ll x, y;
+//	priority_queue <ll, vector<ll>, greater<ll> > q;
+
+int a[2005];
+mii maa;
+int main()
+{
+	fast_input();
+
+}
